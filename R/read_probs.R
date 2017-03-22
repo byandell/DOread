48ee01be151e063e2875527f1b2443ca8da39c8d
Brian Yandell <byandell@wisc.edu>
2017-03-21 19:09:25 -0500

    move RDS based read to read_probs_rds; update to feather

--- R/read_probs.R
+++ R/read_probs.R
@@ -1,13 +1,10 @@
-# Half-way redo of read_probs to align with new R/qtl2 data format
-# calc_genoprob attributes: is_x_chr, alleles, alleleprobs, and crosstype
-
 #' Read genotype probability object from file
 #'
 #' Uses readRDS to read object.
 #'
 #' @param chr vector of chromosome identifiers
+#' @param start_val, end_val start and end values in Mbp
 #' @param datapath name of folder with Derived Data
-#' @param map genome map as list of chromosome maps
 #'
 #' @return list with \code{probs} = large object of class \code{\link[qtl2geno]{calc_genoprob}} and \code{map} = physical map for selected \code{chr}
 #'
@@ -18,29 +15,31 @@
 #' \dontrun{read_probs(chr, datapath)}
 #'
 #' @export
-read_probs <- function(chr=NULL, datapath,
-                       map = readRDS(file.path(datapath, "pmap.rds"))) {
-  read_all_probs <- is.null(chr)
+read_probs <- function(chr=NULL, start_val=NULL, end_val=NULL, datapath) {
 
-  if(!read_all_probs) {
-    ## Read all probs if requesting at least half the chromosomes.
-    read_all_probs <- (2 * length(chr) >= length(map))
-  }
+  map <- readRDS(file.path(datapath, "pmap.rds"))
 
-  if(read_all_probs) {
-    ## Read in phenotype probabilities ("probs") [very large]
-    probs <- readRDS(file.path(datapath, "probs.rds"))
-  } else {
-    ## Read in probs for selected chromosomes and cbind.
-    probs <- readRDS(file.path(datapath, paste0("probs_", chr[1], ".rds")))
-    if(length(chr) > 1) for(chri in chr[-1])
-      probs <- cbind(probs,
-                     readRDS(file.path(datapath, paste0("probs_", chri, ".rds"))))
+  ## Read in phenotype probabilities ("probs") [very large]
+  probs <- readRDS(file.path(datapath, "faprobs.rds"))
+
+  if(!is.null(chr)) {
     map <- map[chr]
+    probs <- probs[, chr]
+
+    if(length(chr) == 1) {
+      if(!is.null(start_val) & !is.null(end_val)) {
+        ## Reduce to region of interest.
+        wh <- which(map[[chr]] >= start_val &
+                      map[[chr]] <= end_val)
+        map[[chr]] <- map[[chr]][wh]
+        probs <- subset(probs1, mar = wh)
+      }
+    }
   }
   list(probs = convert_probs(probs),
        map = map)
 }
+
 #' Read genotype probability object from file
 #'
 #' Uses readRDS to read object.
@@ -61,38 +60,25 @@ read_probs <- function(chr=NULL, datapath,
 #' @export
 #' @rdname read_probs
 #' @importFrom qtl2scan interp_map
-read_probs36 <- function(chr_id, start_val=NULL, end_val=NULL, datapath, map=NULL) {
-  attieDO <- readRDS(file.path(datapath,"attieDO.rds"))  # cross object
-  probs1 <- readRDS(file.path(datapath,
-                              paste0("attieDO_probs", chr_id, ".rds")))
+read_probs36 <- function(chr, start_val=NULL, end_val=NULL, datapath) {
 
-  if(is.null(map)) {
-    map <- probs1$map
-    if(is.null(map))
-      stop("need physical map")
-    map <- qtl2scan::interp_map(map, attieDO$gmap, attieDO$pmap)
-  }
+  if(missing(chr))
+    stop("must specifiy one chromosome")
+  if(length(chr) != 1)
+    stop("must specifiy one chromosome")
 
-  ## Needed for transition only. Does nothing if already in new format.
-  pr <- probs1 <- convert_probs(probs1)
+  map <- readRDS(file.path(datapath, "pmap.rds"))[chr]
+
+  # Read in feather_genoprob object for this chromosome.
+  probs1 <- readRDS(file.path(datapath, paste0("fprobs_", chr, ".rds")))
 
   if(!is.null(start_val) & !is.null(end_val)) {
     ## Reduce to region of interest.
-    wh <- which(map[[chr_id]] >= start_val &
-                  map[[chr_id]] <= end_val)
-    map[[chr_id]] <- map[[chr_id]][wh]
-    probs1[[chr_id]] <- probs1[[chr_id]][,,wh]
+    wh <- which(map[[chr]] >= start_val &
+                  map[[chr]] <= end_val)
+    map[[chr]] <- map[[chr]][wh]
+    probs1 <- subset(probs1, mar = wh)
   }
-
-  ## Fix rownames of probs. Begin with "DO-".
-  tmp <- substring(rownames(probs1[[chr_id]]), 4)
-  rownames(probs1[[chr_id]]) <- tmp
-  ## Sort them in increasing number order.
-  probs1[[chr_id]] <-
-    probs1[[chr_id]][order(as.numeric(tmp)),,]
-
-  # Bring along attributes for calc_genoprob object.
-  probs1 <- modify_probs(pr, probs1)
 
   list(probs = probs1,
        map = map)
---
+++ R/read_probs_rds.R
@@ -0,0 +1,99 @@
+# Half-way redo of read_probs to align with new R/qtl2 data format
+# calc_genoprob attributes: is_x_chr, alleles, alleleprobs, and crosstype
+
+#' Read genotype probability object from file
+#'
+#' Uses readRDS to read object.
+#'
+#' @param chr vector of chromosome identifiers
+#' @param datapath name of folder with Derived Data
+#' @param map genome map as list of chromosome maps
+#'
+#' @return list with \code{probs} = large object of class \code{\link[qtl2geno]{calc_genoprob}} and \code{map} = physical map for selected \code{chr}
+#'
+#' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
+#' @keywords utilities
+#'
+#' @examples
+#' \dontrun{read_probs(chr, datapath)}
+#'
+#' @export
+read_probs_rds <- function(chr=NULL, datapath,
+                       map = readRDS(file.path(datapath, "pmap.rds"))) {
+  read_all_probs <- is.null(chr)
+
+  if(!read_all_probs) {
+    ## Read all probs if requesting at least half the chromosomes.
+    read_all_probs <- (2 * length(chr) >= length(map))
+  }
+
+  if(read_all_probs) {
+    ## Read in phenotype probabilities ("probs") [very large]
+    probs <- readRDS(file.path(datapath, "probs.rds"))
+  } else {
+    ## Read in probs for selected chromosomes and cbind.
+    probs <- readRDS(file.path(datapath, paste0("probs_", chr[1], ".rds")))
+    if(length(chr) > 1) for(chri in chr[-1])
+      probs <- cbind(probs,
+                     readRDS(file.path(datapath, paste0("probs_", chri, ".rds"))))
+    map <- map[chr]
+  }
+  list(probs = convert_probs(probs),
+       map = map)
+}
+#' Read genotype probability object from file
+#'
+#' Uses readRDS to read object.
+#'
+#' @param chr vector of chromosome identifiers
+#' @param start_val, end_val start and end values in Mbp
+#' @param datapath name of folder with Derived Data
+#'
+#' @return list with \code{probs} = large object of class \code{\link[qtl2geno]{calc_genoprob}}
+#'  and \code{map} = physical map for selected \code{chr} region
+#'
+#' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
+#' @keywords utilities
+#'
+#' @examples
+#' \dontrun{read_probs36(chr, start_val, end_val, dirpath)}
+#'
+#' @export
+#' @rdname read_probs_rds
+#' @importFrom qtl2scan interp_map
+read_probs36_rds <- function(chr, start_val=NULL, end_val=NULL, datapath, map=NULL) {
+  attieDO <- readRDS(file.path(datapath,"attieDO.rds"))  # cross object
+  probs1 <- readRDS(file.path(datapath,
+                              paste0("attieDO_probs", chr, ".rds")))
+
+  if(is.null(map)) {
+    map <- probs1$map
+    if(is.null(map))
+      stop("need physical map")
+    map <- qtl2scan::interp_map(map, attieDO$gmap, attieDO$pmap)
+  }
+
+  ## Needed for transition only. Does nothing if already in new format.
+  pr <- probs1 <- convert_probs(probs1)
+
+  if(!is.null(start_val) & !is.null(end_val)) {
+    ## Reduce to region of interest.
+    wh <- which(map[[chr]] >= start_val &
+                  map[[chr]] <= end_val)
+    map[[chr]] <- map[[chr]][wh]
+    probs1[[chr]] <- probs1[[chr]][,,wh]
+  }
+
+  ## Fix rownames of probs. Begin with "DO-".
+  tmp <- substring(rownames(probs1[[chr]]), 4)
+  rownames(probs1[[chr]]) <- tmp
+  ## Sort them in increasing number order.
+  probs1[[chr]] <-
+    probs1[[chr]][order(as.numeric(tmp)),,]
+
+  # Bring along attributes for calc_genoprob object.
+  probs1 <- modify_probs(pr, probs1)
+
+  list(probs = probs1,
+       map = map)
+}
