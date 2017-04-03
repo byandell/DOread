d4b27625b577b085d841a49aece6dbefe5daa6c1
Brian Yandell <byandell@wisc.edu>
2017-04-03 12:28:34 -0500

    fix read_probs_feather to use current datapath to update feather_genoprob object

--- R/read_probs_feather.R
+++ R/read_probs_feather.R
@@ -4,5 +4,14 @@ read_probs_feather <- function(chr, datapath, allele = TRUE) {
   ## Read in feather_genotype object (small).
   probs <- readRDS(file.path(datapath, "feather",
                              ifelse(allele, "faprobs.rds", "fprobs.rds")))
+
+  ## Modify feather directory to match current datapath.
+  pr <- unclass(probs)
+  oldpath <- pr$feather
+  pr$feather <-
+    stringr::str_replace(oldpath,
+                         file.path(".*", "feather", basename(oldpath)),
+                         file.path(datapath, "feather", basename(oldpath)))
+  probs <- modify_object(probs, pr)
   subset(probs, chr = chr)
 }
