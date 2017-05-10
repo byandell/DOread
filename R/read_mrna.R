a9c48456df19f8d6e6fdda5444ca03c450db733d
Brian Yandell <byandell@wisc.edu>
2017-05-10 11:11:56 -0500

    upgrade read_mrna with local and qtl options; fix NAs in QTL column

--- R/read_mrna.R
+++ R/read_mrna.R
@@ -3,10 +3,11 @@
 #' Uses feather to read mrna expression object and associated annotations.
 #'
 #' @param chr_id vector of chromosome identifiers
-#' @param start_val, end_val start and end values in Mbp
+#' @param start_val,end_val start and end values in Mbp
 #' @param datapath name of folder with Derived Data
-#' @param allele read haplotype allele probabilities (if \code{TRUE}) or diplotype allele-pair probabilities (if \code{FALSE})
-#' @param method method of genoprob storage
+#' @param local read only mRNA values local to region if \code{TRUE};
+#' otherwise include distal mRNA values that map to region
+#' @param qtl read only mRNA values with QTL peak in region if \code{TRUE}
 #'
 #' @return list with \code{expr} = matrix of expression mRNA values in region and \code{annot} = data frame of annotations for mRNA.
 #'
@@ -20,24 +21,32 @@
 #' @importFrom dplyr filter group_by inner_join mutate rename summarize ungroup
 #' @importFrom feather read_feather
 #'
-read_mrna <- function(chr_id=NULL, start_val=NULL, end_val=NULL, datapath) {
+read_mrna <- function(chr_id=NULL, start_val=NULL, end_val=NULL, datapath,
+                      local = TRUE, qtl = FALSE) {
 
   start_val6 <- 1e6 * start_val
   end_val6 <- 1e6 * end_val
 
   # Identify mRNA located in region or with QTL peak in region.
   peaks.mrna <- feather::read_feather(file.path(datapath, "RNAseq", "peaks.mrna.feather"))
-  peaks.mrna <- dplyr::mutate(
-    dplyr::filter(peaks.mrna,
-                  ((gene_chr == chr_id &
-                      pmax(gene_start, gene_end) >= start_val6 &
-                      pmin(gene_start, gene_end) <= end_val6) |
-                     (qtl_chr == chr_id &
-                        qtl_pos >= start_val6 &
-                        qtl_pos <= end_val6))),
-    gene_start = 1e-6 * gene_start,
-    gene_end = 1e-6 * gene_end,
-    qtl_pos = 1e-6 * qtl_pos)
+  if(local) {
+    peaks.mrna <- dplyr::filter(peaks.mrna,
+                                gene_chr == chr_id,
+                                pmax(gene_start, gene_end) >= start_val6,
+                                pmin(gene_start, gene_end) <= end_val6)
+  } else {
+    peaks.mrna <- dplyr::filter(peaks.mrna,
+                                ((gene_chr == chr_id &
+                                    pmax(gene_start, gene_end) >= start_val6 &
+                                    pmin(gene_start, gene_end) <= end_val6) |
+                                   (qtl_chr == chr_id &
+                                      qtl_pos >= start_val6 &
+                                      qtl_pos <= end_val6)))
+  }
+  peaks.mrna <- dplyr::mutate(peaks.mrna,
+                              gene_start = 1e-6 * gene_start,
+                              gene_end = 1e-6 * gene_end,
+                              qtl_pos = 1e-6 * qtl_pos)
 
   mrna_ids <- unique(peaks.mrna$gene_id)
 
@@ -62,13 +71,15 @@ read_mrna <- function(chr_id=NULL, start_val=NULL, end_val=NULL, datapath) {
             dplyr::summarize(
               dplyr::group_by(peaks.mrna, gene_id),
               qtl_ct = n(),
+              QTL = paste0(qtl_chr, "@",
+                           round(qtl_pos), ":",
+                           round(lod), collapse = ","),
               qtl_pos = ifelse(any(qtl_chr == chr_id &
                                    qtl_pos >= start_val &
                                    qtl_pos <= end_val),
                                qtl_pos[qtl_chr == chr_id], NA),
-              QTL = paste0(qtl_chr, "@",
-                             round(qtl_pos), ":",
-                             round(lod), collapse = ","))),
+              lod = ifelse(is.na(qtl_pos),
+                           NA, lod))),
           id = gene_id),
         by = "id"),
       local = !is.na(qtl_pos) &
@@ -76,9 +87,19 @@ read_mrna <- function(chr_id=NULL, start_val=NULL, end_val=NULL, datapath) {
         pmax(start,end) >= start_val &
         pmin(start,end) <= end_val)
 
+  if(qtl) {
+    annot.mrna <- dplyr::filter(annot.mrna, !is.na(qtl_pos))
+    # Reduce to mRNA having peak in region.
+    expr_id <- annot.mrna$id
+    peaks.mrna <- dplyr::filter(peaks.mrna, gene_id %in% expr_id)
+  } else {
+    expr_id <- annot.mrna$id
+  }
+
+
   # Get expression data.
   expr.mrna <- as.data.frame(feather::read_feather(file.path(datapath, "RNAseq", "expr.mrna.feather"),
-                                     c("Mouse.ID", annot.mrna$id)))
+                                     c("Mouse.ID", expr_id)))
   rownames(expr.mrna) <- expr.mrna$Mouse.ID
 
   list(expr = expr.mrna[,-1], annot = annot.mrna, peaks = peaks.mrna)
